7b6e5fb158d3ad3a43aa6bb8ef7398eb62282a3c
Anish Karn <anish.karna133@gmail.com>
2022-01-23 17:45:42 +0545

    implement camelCasing

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -20,7 +20,7 @@ const UserPage = () => {
   return (
     <div className="h-full flex flex-col items-center justify-center">
       <div className="bg-orange-200 text-white flex flex-col justify-center items-center border w-72 h-72 m-4 p-3 rounded-full hover:bg-orange-300 hover:cursor-pointer hover:border-blue-500">
-        <h2 className="text-3xl">{`${user?.first_name} ${user?.last_name}`}</h2>
+        <h2 className="text-3xl">{`${user?.firstName} ${user?.lastName}`}</h2>
         <h4 className="text-blue-400">{user?.email}</h4>
       </div>
     </div>
